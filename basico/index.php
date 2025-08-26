24dc50c54b3c31326194100a543e4274f3a75947
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2025-08-26 14:09:33 -0300

    Ajustado exemplos básicos do PHP

--- basico/index.php
+++ basico/index.php
@@ -3,25 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conceitos Básicos PHP</title>
+    <title>Básico lingragem PHP</title>
 </head>
 <body>
-    <h1>
-        <?php 
-           echo "Olá! Eu sou um script PHP rodando no servidor!"
-        ?>
-    </h1>
-    <p>Abaixo trazemos um código gerado no servidor pelo PHP</p>
+    <h1>Conceitos Básicos PHP</h1>
+    <ul>
+        <li>
+            <a href="primeiro.php" target="_blank">Primeiro Script PHP</a>
+        </li>
+    </ul>
+    <br>
     <?php 
-       echo "<h2>Server-side Script com PHP</h2>";
+       echo "<h2>Informações do Servidor</h2>";
+       echo $_SERVER['SCRIPT_NAME'];
        echo "<br>";
-       date_default_timezone_set("America/Sao_Paulo");
-       echo "Data e hora da execução: " . date("H:i:s d/m/Y");
+       echo $_SERVER['SERVER_NAME'];
+       echo "<br>";
     ?>
-    <script language="javascript">
-        document.write("<h2>Script do lado do cliente com JavaScript</h2>");
-        document.write("<p>Este é um script que roda no navegador do usuário.</p>");
-        document.write("<p>Data e hora local: " + new Date().toLocaleString() + "</p>");
-    </script> 
 </body>
 </html>
